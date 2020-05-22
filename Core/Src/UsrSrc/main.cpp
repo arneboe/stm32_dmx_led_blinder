d824c4c5f6f42dbc7d70067e99b72da52bc1c235
arne <arne@rnae.de>
2020-05-22 17:23:17 +0200

    initial commit

---
+++ Core/Src/UsrSrc/main.cpp
@@ -0,0 +1,20 @@
+/*
+ * main.cpp
+ *
+ *  Created on: May 22, 2020
+ *      Author: arne
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** This is the user entry method. It is call forever from main() */
+void loop()
+{
+
+}
+
+#ifdef __cplusplus
+}
+#endif
